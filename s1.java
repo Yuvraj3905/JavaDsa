f1753b5bd4c9fac2649c9a2dfb992f67e6716820
yuvraj3905 <yuvraj202001@gmail.com>
2022-05-24 11:29:38 +0530

    28 days

---
+++ s1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class s1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String st=scan.nextLine(); 
+        // Write your code here.
+
+        System.out.println(st);
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
